79588ae1236a9dd0139d387bfe72db8a5d780418
Levi Lesches <levilesches@gmail.com>
2023-11-08 05:03:43 -0500

    Updated logging

--- lib/logging.dart
+++ lib/logging.dart
@@ -3,40 +3,30 @@ library;
 
 import "dart:io";
 import "package:logger/logger.dart";
+export "package:logger/logger.dart";
 
 /// An alias for [Level].
 typedef LogLevel = Level;
 
-/// Holds the current [LogLevel] for [logger].
-class BurtLogger {
-  /// The current [LogLevel] for [logger].
-  static LogLevel level = LogLevel.info;
-}
-
-/// A custom filter to work around a bug with `package:logger`.
-/// 
-/// See https://github.com/Bungeefan/logger/issues/38.
-class BurtFilter extends LogFilter {
-  @override 
-  bool shouldLog(LogEvent event) => event.level.index >= BurtLogger.level.index;
-}
-
 /// The logger to use when running BURT programs. 
 /// 
-/// See [LoggerUtils] for usage. To change the minimum log level, use [BurtLogger.level].
-final logger = Logger(printer: SimplePrinter(colors: stdout.supportsAnsiEscapes), filter: BurtFilter());
+/// See [LoggerUtils] for usage. To change the minimum log level, use [Logger.level].
+final logger = Logger(
+  printer: SimplePrinter(colors: stdout.supportsAnsiEscapes), 
+  filter: ProductionFilter(),
+);
 
 /// Helpful aliases for the [Logger] class.
 extension LoggerUtils on Logger {
   /// Logs a debug message.
   /// 
   /// Use this to print values you want to inspect later.
-  void debug(String message) => d(message);
+  void trace(String message) => t(message);
 
   /// Logs a verbose message.
   /// 
-  /// Use this to print status updates more often than [info].
-  void verbose(String message) => t(message);
+  /// Use this to print status updates that can help debugging.
+  void debug(String message) => d(message);
 
   /// Logs an info message.
   /// 
@@ -47,11 +37,6 @@ extension LoggerUtils on Logger {
   /// 
   /// Use this to indicate something has gone wrong but can be recovered.
   void warning(String message) => w(message);
-
-  /// Logs an error message.
-  /// 
-  /// Use this when an error occurs in a try/catch block. This is like a verbose [critical].
-  void error(String message, Object error, [StackTrace? stackTrace]) => e(message, error: error, stackTrace: stackTrace);
 
   /// Logs a critical message.
   /// 
