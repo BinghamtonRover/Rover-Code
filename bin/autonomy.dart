9b4bd73e82aa585163641c6223f75994466a60d9
Levi Lesches <levilesches@gmail.com>
2024-05-16 18:58:54 -0400

    Added simulator and real rover logic (#1)
    This PR splits the code into 3 main categories: 
    - interfaces: an interface for each component that defines what it can do
    - simulated: a simulated version for each component
    - rover: the real implementation of each component
    
    Using this new structure, this PR also adds unit tests that simulate all
    but one component at a time. These tests are run for every new PR and
    commit.

--- bin/autonomy.dart
+++ bin/autonomy.dart
@@ -1,8 +1,6 @@
-import "package:autonomy/autonomy.dart";
-import "package:burt_network/logging.dart";
+import "package:autonomy/rover.dart";
 
-void main(List<String> arguments) async {
-  final tankMode = arguments.contains("--tank");
-  if (tankMode) logger.info("Running in tank mode");
-  await collection.init(tankMode: arguments.contains("--tank"));
+void main() async {
+  final rover = RoverAutonomy();
+  await rover.init();
 }
--- lib/autonomy.dart
+++
@@ -1,4 +0,0 @@
-export "src/collection.dart";
-export "src/drive.dart";
-export "src/server.dart";
-export "src/subsystems.dart";
