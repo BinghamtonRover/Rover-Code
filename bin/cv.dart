d6fea27d548905045b06dc4b28966496da26e7a5
Artin <artintonekaboni@gmail.com>
2024-11-18 20:55:28 -0500

    setup

---
+++ bin/cv.dart
@@ -0,0 +1,5 @@
+import 'package:cv/cv.dart' as cv;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cv.calculate()}!');
+}
---
+++ test/cv_test.dart
@@ -0,0 +1,8 @@
+import 'package:cv/cv.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
