900b278e085580773019ecb7c5df784cb3bea098
unknown <aadij0415@gmail.com>
2025-01-16 17:36:27 -0500

    augmented model

--- bin/trainer.py
+++ bin/trainer.py
@@ -11,21 +11,11 @@ class Trainer:
 	def save_model(self):
 		self.model.save("trained-model.pt")
 
-	def download_dataset(self):
-		load_dotenv()
-		api_key = os.getenv("ROBOFLOW_API_KEY")
-
-		# Download a dataset
-		rf = Roboflow(api_key=api_key)
-		project = rf.workspace("binghamtonrover").project("burt-test")
-		version = project.version(1)
-		dataset = version.download("yolov11")
-
 	def train_model(self):
 		# Load a model
 		# Models available are: yolo11n, yolov11s.pt, yolov11m.pt, yolov11l.pt, yolov11x.pt
 		self.model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
-		results = self.model.train(data="BURT-Test-1/data.yaml", epochs = 75, imgsz = 256,  auto_augment = "randaugment", crop_fraction=0.5, degrees = 90, shear = 90)
+		results = self.model.train(data="Mallet Images.v1i.yolov11\data.yaml", epochs = 75, imgsz = 256,  auto_augment = "randaugment", crop_fraction=0.5, degrees = 90, shear = 90)
 		print(results)
 
 	def validate_model(self):
