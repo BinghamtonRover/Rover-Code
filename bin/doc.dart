9ff2f9a5ccaf8b1b603d2f5f73c8bd1db3823592
Levi Lesches <levilesches@gmail.com>
2025-01-24 06:08:10 -0500

    Added `dart run :docs -p <package>` to generate documentation (#6)
    Running this command with one of the Dart packages will generate
    documentation for it and provide the link to open in your browser.

---
+++ bin/doc.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: avoid_print
+
+import "dart:io";
+
+import "package:args/args.dart";
+import "package:pubspec_parse/pubspec_parse.dart";
+import "package:dhttpd/dhttpd.dart";
+import "package:cli_spin/cli_spin.dart";
+
+void main(List<String> args) async {
+  print("");
+
+  // Get list of valid Dart packages
+  final pubspecFile = File("pubspec.yaml");
+  final pubspec = Pubspec.parse(await pubspecFile.readAsString());
+  final packages = pubspec.workspace!;
+
+  // Get the user's choice of package
+  final parser = ArgParser()
+    ..addOption("package", abbr: "p", mandatory: true);
+  final results = parser.parse(args);
+  final package = results.option("package");
+  if (package == null) {
+    print("Usage: dart run :doc -p <package>");
+    print(parser.usage);
+    return;
+  } else if (!packages.contains(package)) {
+    print("Please choose one of the following packages: ");
+    print("  ${packages.join(", ")}");
+    return;
+  }
+
+  // Document the package
+  final spinner = CliSpin(text: "Documenting $package...", color: CliSpinnerColor.white);
+  spinner.start();
+  const docCommand = "dart";
+  final docArgs = ["doc", package, "-o", "docs"];
+  final result = await Process.run(docCommand, docArgs);
+  if (result.exitCode != 0) {
+    spinner.fail("Could not document $package");
+    print(result.stderr);
+    return;
+  }
+  spinner.success("Done!");
+
+  // Open the docs
+  await Dhttpd.start(path: "docs", port: 8888);
+  print("Serving documentation at http://localhost:8888");
+  print("Press CTRL+C to exit.");
+}
