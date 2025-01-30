8ad4f5a73eeb94858438fbdedc2bb5d1fb520c11
aidan ahram <aidanahram1@gmail.com>
2025-01-29 21:50:01 -0500

    fix bash file

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
-mkdir build
-cd build && cmake ../src && cmake --build . -j
+#!/bin/bash
+
+cd src
+# git submodule update
+cd ..
+
+# TODO: Check if it exists first
+#  If so, don't make it
+if [ ! -d "$DIRECTORY" ]; then
+  mkdir build
+fi
+
+cd build
+cmake ../src
+cmake --build . -j 1
+cd ..
