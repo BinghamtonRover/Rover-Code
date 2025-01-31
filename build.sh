ffbd3dd864e207214c3a5cc02c6a545daa9b1cfb
Hewitt <wangh3@bxscience.edu>
2025-01-31 02:17:58 -0500

    build script not reliant on cmake (as much)

--- build.sh
+++ build.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-cd src
-# git submodule update
-cd ..
+# exit on any error (i was not aware of this)
+set -e
+export ROS_VERSION=0
+export CXXFLAGS=-fPIC
 
-# TODO: Check if it exists first
-#  If so, don't make it
-if [ ! -d "$DIRECTORY" ]; then
-  mkdir build
-fi
-
+mkdir -p build
 cd build
-cmake ../src
-cmake --build . -j 1
-cd ..
+
+echo "building sick_scan_xd..."
+cmake ../src/sick_scan_xd/
+make -j
+
+pwd
+
+echo "building lidar_ffi..."
+
+g++ -fPIC -shared \
+    -I../src/sick_scan_xd/include \
+    ../src/lidar.cpp \
+    -o liblidar_ffi.so \
+    ./libsick_scan_xd_lib.a
+
+mkdir -p include
+cp ../src/lidar.h include/
+cp ../src/sick_scan_api.h include/
+
+echo "Done."
