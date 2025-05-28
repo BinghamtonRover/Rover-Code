fdb3ae2b819e2b335b57a7e80706f73956de1f7f
niooii <121910815+niooii@users.noreply.github.com>
2025-05-28 17:09:54 -0400

    Cv cuda (#41)
    - Adds Cuda support for the jetson to enable multi-camera object detection and falls back to CPU if Cuda is not supported.
    - cleans up the old training files and updates the CV model to the best model.

--- vision/lib/analyzer.py
+++ vision/lib/analyzer.py
@@ -1,9 +1,16 @@
 import cv2
 from ultralytics import YOLO
+import torch
 
 class ImageAnalyzer:
   def __init__(self, load_from="trained-model.pt"):
-    self.model = YOLO(load_from, verbose=False)
+    torch.cuda.set_device(0)
+    if torch.cuda.is_available():
+        torch.cuda.set_device(0)
+        self.device = "cuda"
+    else:
+        self.device = "cpu"
+        print("CUDA not available - using CPU")
 
   def has_mallet(self, frame: cv2.Mat, confidence = 0.80) -> bool:
     results = self.model.predict(frame, stream=True, verbose=False)
@@ -11,8 +18,7 @@ class ImageAnalyzer:
       json = result.summary()
       if not json: return False
       object = json[0]
-      print(f"Confidence in analyzer: {object['confidence']}")
-      is_mallet = object['name'] == "mallet" and object['confidence'] > confidence
+      is_mallet = ("mallet" in object['name'].lower()) and object['confidence'] > confidence
       print("Confidence: " + str(object['confidence']))
       if is_mallet: 
         return True
@@ -41,7 +47,7 @@ class ImageAnalyzer:
     return frame
 
 if __name__ == "__main__":
-  camera = ImageAnalyzer()
+  camera = ImageAnalyzer(load_from="trained.pt")
   cam = cv2.VideoCapture(0)
   while True:
     camera.getFrames(cam, show = True)
