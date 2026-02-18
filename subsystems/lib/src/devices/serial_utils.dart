06b910241f4ee177eb07dcdb646be1d358512eb5
Gold87 <91761103+Gold872@users.noreply.github.com>
2026-02-18 13:24:56 -0500

    Filter out all rover paths in getPortNames()

--- subsystems/lib/src/devices/serial_utils.dart
+++ subsystems/lib/src/devices/serial_utils.dart
@@ -9,13 +9,31 @@ import "package:subsystems/subsystems.dart";
 Future<String> getRealPath(String symlink) async =>
     (await Process.run("realpath", [symlink])).stdout.trim();
 
+/// Gets a list of all /dev/rover_ paths
+Future<List<String>> getRoverPaths() async {
+  final devDir = Directory("/dev");
+  const prefix = "/dev/rover_";
+
+  final output = <String>[];
+
+  await for (final entity in devDir.list()) {
+    if (entity.path.startsWith(prefix)) {
+      output.add(entity.path);
+    }
+  }
+  return output;
+}
+
 /// Gets all the names of all the ports.
 Future<Iterable<String>> getPortNames() async {
   final allPorts = DelegateSerialPort.allPorts.toSet();
   if (!Platform.isLinux) return allPorts;
-  final imuPath = await getRealPath(imuPort);
-  final gpsPath = await getRealPath(gpsPort);
-  final forbiddenPorts = {imuPath, gpsPath, "/dev/ttyAMA10"};
+
+  final forbiddenPorts = {"/dev/ttyAMA10"};
+  final roverPaths = await getRoverPaths();
+  for (final path in roverPaths) {
+    forbiddenPorts.add(await getRealPath(path));
+  }
   return allPorts.toSet().difference(forbiddenPorts);
 }
 
